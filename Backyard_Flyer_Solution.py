95167bbb8501c2c96ea874c9d11a6281a9e96837
Uljan Sinani <uljan89@gmail.com>
2020-12-19 23:04:35 +0000

    comleted state_callback

--- Backyard_Flyer_Solution.py
+++ Backyard_Flyer_Solution.py
@@ -3,6 +3,7 @@ import time
 from enum import Enum
 
 import numpy as np
+from numpy.linalg import norm as distance
 
 from udacidrone import Drone
 from udacidrone.connection import MavlinkConnection, WebSocketConnection  # noqa: F401
@@ -16,8 +17,7 @@ class States(Enum):
     WAYPOINT = 3
     LANDING = 4
     DISARMING = 5
-
-
+    
 class BackyardFlyer(Drone):
 
     def __init__(self, connection):
@@ -29,19 +29,49 @@ class BackyardFlyer(Drone):
 
         # initial state
         self.flight_state = States.MANUAL
-
-        # TODO: Register all your callbacks here
         self.register_callback(MsgID.LOCAL_POSITION, self.local_position_callback)
         self.register_callback(MsgID.LOCAL_VELOCITY, self.velocity_callback)
         self.register_callback(MsgID.STATE, self.state_callback)
 
     def local_position_callback(self):
         """
-        TODO: Implement this method
+        @note On position-dependent transitions, use this
 
         This triggers when `MsgID.LOCAL_POSITION` is received and self.local_position contains new data
         """
-        pass
+
+        # if the drone is in flight state
+        if self.flight_state == States.TAKEOFF:
+            # compare the local position with target position
+            
+            if -1.0 * self.local_position[2] > 0.90 * self.target_position[2]:
+
+                # start the path
+                self.all_waypoints = self.calculate_box()
+
+                # check if there is a path to take
+                if len(self.all_waypoints) > 0:
+                    self.waypoint_transition()
+                else:
+                    self.landing_transition()
+        
+        elif self.flight_state == States.WAYPOINT:
+            print(self.target_position)
+            print(self.local_position)
+            cur_pos = self.local_position
+            cur_pos[2] *= -1
+
+            dist = abs(distance(self.target_position - cur_pos))
+            print("dist: {}".format(dist))
+            # In case its area of operation is within .5 meters
+            if dist < 0.5 and self.local_velocity[0] < 0.1 and self.local_velocity[1] < 0.1:
+                if len(self.all_waypoints) > 0:
+                    self.waypoint_transition()
+                else:
+                    self.landing_transition()
+
+
+
 
     def velocity_callback(self):
         """
@@ -49,7 +79,10 @@ class BackyardFlyer(Drone):
 
         This triggers when `MsgID.LOCAL_VELOCITY` is received and self.local_velocity contains new data
         """
-        pass
+        if self.flight_state == States.LANDING:
+            imu_alt = -1.0 * self.local_position[2]
+            if (abs(self.global_positions[2] - self.global_home[2]) < 0.1 and imu_alt < 0.1):
+                self.disarming_transition()
 
     def state_callback(self):
         """
@@ -57,14 +90,32 @@ class BackyardFlyer(Drone):
 
         This triggers when `MsgID.STATE` is received and self.armed and self.guided contain new data
         """
-        pass
+        if not self.in_mission: return
+        if self.flight_state == States.MANUAL:
+            self.arming_transition()
+        elif self.flight_state == States.ARMING and self.armed:
+            self.takeoff_transition()
+        if self.flight_state == States.DISARMING and not self.armed:
+            self.manual_transition()
+            
 
     def calculate_box(self):
         """TODO: Fill out this method
         
         1. Return waypoints to fly a box
         """
-        pass
+        box_length = 10
+        cn = self.local_position[0]
+        ce = self.local_position[1]
+        ca = -1 * self.local_position[2]
+        return [
+                np.array([cn + box_length, ce, ca]),
+                np.array([cn + box_length, ce + box_lenth, ca]),
+                np.array([cn, ce + box_length, ca]),
+                np.array([cn, ce, ca])
+
+                ]
+
 
     def arming_transition(self):
         """TODO: Fill out this method
